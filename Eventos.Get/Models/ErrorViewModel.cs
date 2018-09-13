97f502bf9ba64ee8061f11a19940103010c965d4
Marcos Totte <tottejunior@gmail.com>
2018-09-13 22:05:35 +0100

    Check-in inicial do módulo Eventos.Get

---
+++ Eventos.Get/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eventos.Get.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
